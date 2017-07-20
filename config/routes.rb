9c8ef34030d555dd54ad269327076873060fe5dc
azinck94 <Andrew_zinck@student.uml.edu>
2017-07-20 16:35:20 -0400

    created a controller for my players created controller for players in controllers/players_controller.rb. It will take params of params.require(:player).permit(:first_name, :last_name, :team, :position, :buckets, :rb_per_game, :asst_per_game, :sponsors, :shoes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  resources :examples, except: [:new, :edit]
+  resources :players
+  resources :brands
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+  resources :examples, except: [:new, :edit]
+  resources :brands, only: [:index, :show, :destroy, :update]
+  resources :players, only: [:index, :show, :destroy, :update]
 end
